4b609008fe436980a97551b9033fb03e2be4674b
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-03-17 15:40:55 +0900

    update RQ1 bubble plot

--- src/RQ1/contribution_vs_methodology_bubble.r
+++ src/RQ1/contribution_vs_methodology_bubble.r
@@ -7,15 +7,13 @@ dataset <- read.csv("data/research_type.csv", header=TRUE, sep=",")
 
 contributions <- c("Other", "Communication", "Quality assurance","Program comprehension","Automation","Potential benefits", "Socio-technical effects",   "Understanding")
 
-g <- ggplot(dataset, aes(x=Category, y=Methodology, size=as.numeric(size),colour=Subclass)) + 
-geom_point() +
-scale_size_continuous(range=c(1,20), name="Count", breaks=c(0, 10, 20, 30)) +
-scale_x_discrete(limits=contributions)+
-scale_colour_grey() +
-theme_light() + 
+g <- ggplot(dataset, aes(x=Category, y=Methodology, size=as.numeric(size), label=as.numeric(size))) + 
+geom_point(shape = 21, fill = "white") +
+geom_text(size=4)+
+ scale_size(range = c(5, 20), guide = F) +
 theme(axis.title=element_text(size=18),
 axis.text=element_text(size=15),
-axis.text.x=element_text(angle=30, hjust=1, vjust=1)
-)+
+axis.text.x=element_text(angle=30, hjust=1, vjust=1))+
+facet_grid(~Subclass, scales = "free_x") +
 xlab("Contribution")
 plot(g)
